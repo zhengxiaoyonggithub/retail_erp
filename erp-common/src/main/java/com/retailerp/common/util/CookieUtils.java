679abfe4282da85681e9a556faa095ab4afb96be
zhengxiaoyong <yong_notepad@163.com>
2018-10-25 17:52:02 +0800

    commit

---
+++ erp-common/src/main/java/com/retailerp/common/util/CookieUtils.java
@@ -0,0 +1,48 @@
+package com.retailerp.common.util;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
+public final class CookieUtils {
+
+	/**
+	 * @param request
+	 * @param cookieName
+	 * @return
+	 */
+	public static String getCookieValue(HttpServletRequest request, String cookieName) {
+		return getCookieValue(request, cookieName, false);
+	}
+
+	/**
+	 * @param request
+	 * @param cookieName
+	 * @param isDecoder
+	 * @return
+	 */
+	public static String getCookieValue(HttpServletRequest request, String cookieName, boolean isDecoder) {
+	      Cookie[] cookieList = request.getCookies();
+	        if (cookieList == null || cookieName == null) {
+	            return null;
+	        }
+	        String retValue = null;
+	        try {
+	            for (int i = 0; i < cookieList.length; i++) {
+	                if (cookieList[i].getName().equals(cookieName)) {
+	                    if (isDecoder) {
+	                        retValue = URLDecoder.decode(cookieList[i].getValue(), "UTF-8");
+	                    } else {
+	                        retValue = cookieList[i].getValue();
+	                    }
+	                    break;
+	                }
+	            }
+	        } catch (UnsupportedEncodingException e) {
+	            e.printStackTrace();
+	        }
+	        return retValue;
+	}
+}
---
+++ erp-manager-web/src/main/java/com/retailerp/manager/interceptor/LoginInterceptor.java
@@ -0,0 +1,49 @@
+package com.retailerp.manager.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.retailerp.common.util.CookieUtils;
+
+public class LoginInterceptor implements HandlerInterceptor {
+
+	
+	@Value("${LOGIN_COOKIE_NAME}")
+	private String LOGIN_COOKIE_NAME;
+	
+	@Value("${SSO_URL}")
+	private String SSO_URL;
+	
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		String loginInfo = CookieUtils.getCookieValue(request, LOGIN_COOKIE_NAME);
+		
+		if(StringUtils.isBlank(loginInfo)) {
+			response.sendRedirect(SSO_URL + "/page/login?redirect=" + request.getRequestURL());
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+		// TODO Auto-generated method stub
+		HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
+	}
+
+	@Override
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
+			throws Exception {
+		// TODO Auto-generated method stub
+		HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
+	}
+
+	
+}
